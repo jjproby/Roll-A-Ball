8fba35bf6e647a94b41b7230a02e4f2428ad5d75
Jluedtke19 <jluedtke19@cornellcollege.edu>
2018-03-20 14:54:38 -0500

    Jakes Changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private int count;
 
+    public AudioClip pickupSound;
+    private AudioSource source;
+
 
     private void Start()
     {
@@ -19,6 +22,8 @@ public class PlayerController : MonoBehaviour {
         count = 0;
         SetCountText();
         winText.text = "";
+
+        source = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -38,6 +43,7 @@ public class PlayerController : MonoBehaviour {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
+            source.PlayOneShot(pickupSound);
         }
     }
 
